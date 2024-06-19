01e02a9d97f03211af2ad9a2f46de0aecfe4a7ab
Vitor F. Morais <111707785+vmorais111@users.noreply.github.com>
2024-06-19 10:05:41 -0300

    cad

--- SO2-prototipo/frontend/src/components/cadastro.tsx
+++ SO2-prototipo/frontend/src/components/cadastro.tsx
@@ -1,21 +1,61 @@
 
 import MenuSuperior from './menuSuperior'
-import { Salas } from '../usefulBits/salasInterface'
 import { useState } from 'react'
+import { Salas } from '../usefulBits/salasInterface'
+import dbConnection from '../usefulBits/dbConnection'
+import axios from 'axios'
 
 
 function Cadastro() {
   
-  const [salaCadastrar,setSalaCadastrar] = useState<Salas>();
   const [nome,setNome] = useState<string>("")
   const [local,setLocal] = useState<string>("")
-  const [dia,setDia] = useState<Date>();
+  const [dia,setDia] = useState<string>();
   const [horaInicial,setHoraInicial] = useState<string>("")
   const [horaFinal,setHoraFinal] = useState<string>("")
   const [respons,setRepons] = useState<string>("")
   const [motivo,setMotivo] = useState<string>("")
   const [aditionalInfo,setAditionalInfo] = useState<string>("")
   const [convidados,setConvidados] = useState<string[]>([])
+  const [convidado,setConvidado] = useState<string>("")
+
+  const handleAddConv = () =>{
+    setConvidados([...convidados,convidado])
+  }
+
+  const convertToDate:any = (e:string) => {
+    const a = new Date(e)
+    return a
+  }
+
+
+
+  async function handleSubmit(e:any){
+
+    const Model: Salas ={
+      salaId: 3,
+      salaNome: nome,
+      caminhoImagem: "",
+      salaLocal: local,
+      salaDataUso: convertToDate(dia),
+      salaHoraInicio: horaInicial,
+      salaHoraFinal: horaFinal,
+      salaResponsavel: respons,
+      salaMotivo: motivo,
+      salaInfo: aditionalInfo,
+      salaConvidados: JSON.parse(JSON.stringify(convidados))
+    }
+    
+    e.preventDefault();
+    const url = `${dbConnection}/salas`
+
+    try {
+      const response = await axios.post(url,Model)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
 
   return (
@@ -30,16 +70,17 @@ function Cadastro() {
           <div className=' h-[23rem] w-80 border-2 border-slate-400 rounded'>
             <div className='h-full p-5 flex flex-col justify-center '>
             <form className='flex flex-col gap-1'>
-              <input type="text" placeholder='Nome'/>
-              <input type="text" placeholder='Local'/>
-              <input type="date" placeholder='Dia'/>
-              <input type="text" placeholder='Hora inicial (hh:mm)'/>
-              <input type="text" placeholder='Hora final (hh:mm)'/>
-              <input type="text" placeholder='Responsável'/>
-              <input type="text" placeholder='Motivo'/>
-              <input type="text" placeholder='informações adicionais'/>
-              <input type="text" placeholder='Convidados'/>
-              <button type="button" className='bg-slate-600 text-white rounded-lg mt-2'>Cadastrar</button>
+              <input type="text" placeholder='Nome' onChange={(e)=>setNome(e.target.value)}/>
+              <input type="text" placeholder='Local' onChange={(e)=>setLocal(e.target.value)}/>
+              <input type="date" placeholder='Dia' onChange={(e)=>setDia(e.target.value)}/>
+              <input type="text" placeholder='Hora inicial (hh:mm)'onChange={(e)=>setHoraInicial(e.target.value)}/>
+              <input type="text" placeholder='Hora final (hh:mm)' onChange={(e)=>setHoraFinal(e.target.value)}/>
+              <input type="text" placeholder='Responsável' onChange={(e)=>setRepons(e.target.value)}/>
+              <input type="text" placeholder='Motivo' onChange={(e)=>setMotivo(e.target.value)}/>
+              <input type="text" placeholder='informações adicionais' onChange={(e)=>setAditionalInfo(e.target.value)}/>
+              <input type="text" placeholder='Convidados' onChange={(e)=>{setConvidado(e.target.value)}}/>
+              <button type="button"  className='bg-slate-600 text-white rounded-lg mt-2' onClick={handleAddConv}>Adicionar convidado</button>
+              <button type="button" className='bg-slate-600 text-white rounded-lg mt-2' onClick={handleSubmit}>Cadastrar</button>
             </form>
             </div>
 
